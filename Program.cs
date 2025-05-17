8b9cba18cf2ba2fc6b2cb0314f289926827bd982
Diego Neri <diego.neri@etec.sp.gov.br>
2025-05-16 21:34:34 -0300

    Finaliza exercício

--- Program.cs
+++ Program.cs
@@ -7,11 +7,22 @@ notaUm = SolicitaNota(1);
 notaDois = SolicitaNota(2);
 notaTres = SolicitaNota(3);
 notaQuatro = SolicitaNota(4);
-if (!ValidaNotas(N1: notaUm, N2: notaDois, N3: notaTres, N4: notaQuatro))
+
+bool saoNotasValidas = ValidaNotas(N1: notaUm, N2: notaDois, N3: notaTres, N4: notaQuatro);
+if (!saoNotasValidas)
 {
     Console.WriteLine("Digite somente notas entre 0 e 10.");
     Environment.Exit(0);
 }
+
+media = (notaUm + notaDois + notaTres + notaQuatro) / 4;
+
+resultado = DefineResultado(media);
+
+// Exibir o resultado em uma subrotina
+// Exibir cores do resultado de acordo com o valor
+// aprovado = verde / reprovado = vermelho / em recuperação = amarelo
+Console.WriteLine($"Você ficou com média {media:N1}. Resultado: {resultado}");
 
 void ExibeBoasVindas()
 {
@@ -40,4 +51,32 @@ bool ValidaNota(double nota)
 {
     return nota >= 0 && nota <= 10;
     //false quando houver alguma nota < 0 ou nota > 10
+}
+
+string DefineResultado(double media)
+{
+    //return media < 5 ? "Reprovado" : "Aprovado";
+    // if (media < 5)
+    // {
+    //     return "Reprovado";
+    // }
+    // else
+    // {
+    //     return "Aprovado";
+    // }    
+    return media < 5 ? "Reprovado" : 
+           media < 6 ? "Em Recuperação" : 
+           "Aprovado";
+    // if (media < 5)
+    // {
+    //     return "Reprovado";
+    // }
+    // else if (media < 6)
+    // {
+    //     return "Em recuperação";
+    // }
+    // else
+    // {
+    //     return "Aprovado";
+    // }     
 }
